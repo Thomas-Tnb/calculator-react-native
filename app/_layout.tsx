fe3ce0ac9eaa21d44fd07ecef263d15056959ca6
Thomas-Tnb <thomas.jt2005@gmail.com>
2026-02-26 22:14:44 -0300

    armazenando expressao e identificando n1,n2 e op

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,29 +3,48 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  const [primeiroNum, setPrimeiroNum] = useState("0");
-  const [segundoNum, setSegundoNum] = useState("");
+  const [expressao, setExpressao] = useState("0");
   const [operador, setOperador] = useState("");
-  const [opExiste, setOpExiste] = useState(false);
 
-  const calculaExpressao = (n1, op, n2) => {
-    const num1 = Number(n1);
-    const num2 = Number(n2);
+  const operadores = ["+", "-", "X", "/"];
+  // se há algum operador ele substitui, senão adiciona à expressão
+  const adicionarOperador = (op) => {
+    setOperador(op);
+    setExpressao((valorAtual) => {
+      const jaTemOperador = operadores.some((operador) =>
+        valorAtual.includes(operador),
+      );
+      if (jaTemOperador) {
+        return valorAtual.replace(/[+\-X/]/, op);
+      }
+
+      return valorAtual + op;
+    });
+  };
+
+  const calculaExpressao = (expr, op) => {
+    const indiceOp = expr.indexOf(op);
+    const num1 = Number(expr.substring(0, indiceOp));
+    const num2 = Number(expr.substring(indiceOp + 1));
 
     switch (op) {
       case "+":
+        setExpressao(String(num1 + num2));
         return num1 + num2;
 
       case "-":
+        setExpressao(String(num1 - num2));
         return num1 - num2;
 
       case "X":
+        setExpressao(String(num1 * num2));
         return num1 * num2;
 
       case "/":
         if (num2 === 0) {
           return "Erro";
         }
+        setExpressao(String(num1 / num2));
         return num1 / num2;
 
       default:
@@ -36,46 +55,48 @@ export default function RootLayout() {
   return (
     <SafeAreaView>
       <View>
-        <Text>
-          {primeiroNum}
-          {operador}
-          {segundoNum}
-        </Text>
+        <Text>{expressao}</Text>
       </View>
 
-      {["+", "-", "X", "/"].map((op) => (
+      {/* OPERADORES */}
+      {operadores.map((op) => (
         <TouchableOpacity
           key={op}
           style={styles.teclaOperador}
           onPress={() => {
-            setOperador(op);
-            setOpExiste(true);
+            adicionarOperador(op);
           }}
         >
           <Text>{op}</Text>
         </TouchableOpacity>
       ))}
 
+      {/* TECLADO NUMERICO */}
       {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((numero) => (
         <TouchableOpacity
           key={numero}
           style={styles.teclaNumerica}
           onPress={() => {
-            if (!opExiste) {
-              if (primeiroNum === "0") {
-                setPrimeiroNum(String(numero));
-              } else {
-                setPrimeiroNum(primeiroNum + String(numero));
-              }
+            if (expressao === "0") {
+              setExpressao(String(numero));
             } else {
-              setSegundoNum(segundoNum + String(numero));
+              setExpressao(expressao + String(numero));
             }
           }}
         >
           <Text>{numero}</Text>
         </TouchableOpacity>
       ))}
-      <TouchableOpacity></TouchableOpacity>
+
+      <TouchableOpacity onPress={() => setExpressao(expressao.slice(0, -1))}>
+        <Text>{"<-"}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => calculaExpressao(expressao, operador)}>
+        <Text>{"="}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => setExpressao("0")}>
+        <Text>{"Limpar"}</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
