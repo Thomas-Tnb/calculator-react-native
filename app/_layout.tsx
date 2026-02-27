905d1b6324e155275e58fcae44cdbf1f3d8287b2
Thomas-Tnb <thomas.jt2005@gmail.com>
2026-02-26 21:15:18 -0300

    add exibicao de teclas + comeco da logica

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,102 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const [primeiroNum, setPrimeiroNum] = useState("0");
+  const [segundoNum, setSegundoNum] = useState("");
+  const [operador, setOperador] = useState("");
+  const [opExiste, setOpExiste] = useState(false);
+
+  const calculaExpressao = (n1, op, n2) => {
+    const num1 = Number(n1);
+    const num2 = Number(n2);
+
+    switch (op) {
+      case "+":
+        return num1 + num2;
+
+      case "-":
+        return num1 - num2;
+
+      case "X":
+        return num1 * num2;
+
+      case "/":
+        if (num2 === 0) {
+          return "Erro";
+        }
+        return num1 / num2;
+
+      default:
+        return "Operador inválido";
+    }
+  };
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaView>
+      <View>
+        <Text>
+          {primeiroNum}
+          {operador}
+          {segundoNum}
+        </Text>
+      </View>
+
+      {["+", "-", "X", "/"].map((op) => (
+        <TouchableOpacity
+          key={op}
+          style={styles.teclaOperador}
+          onPress={() => {
+            setOperador(op);
+            setOpExiste(true);
+          }}
+        >
+          <Text>{op}</Text>
+        </TouchableOpacity>
+      ))}
+
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((numero) => (
+        <TouchableOpacity
+          key={numero}
+          style={styles.teclaNumerica}
+          onPress={() => {
+            if (!opExiste) {
+              if (primeiroNum === "0") {
+                setPrimeiroNum(String(numero));
+              } else {
+                setPrimeiroNum(primeiroNum + String(numero));
+              }
+            } else {
+              setSegundoNum(segundoNum + String(numero));
+            }
+          }}
+        >
+          <Text>{numero}</Text>
+        </TouchableOpacity>
+      ))}
+      <TouchableOpacity></TouchableOpacity>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  teclaNumerica: {
+    width: 30,
+    alignItems: "center",
+    marginHorizontal: 15,
+    padding: 10,
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: "#f8e800ff",
+  },
+  teclaOperador: {
+    width: 50,
+    alignItems: "center",
+    marginHorizontal: 15,
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "rgb(0, 70, 248)",
+  },
+});
